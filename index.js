49d4b81cf521bd81260e4c9b45cc61167cac0114
patrick-rush <patrick.a.rush@gmail.com>
2020-12-12 19:28:37 -0500

    commit tree 2c39624d1b0a9886e4960b62d15dfe25fdb06062

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(superbowls) {
+  let win = superbowls.find( function(e) { return e.result === "W" })
+  if (win) {
+    return win.year
+  }
+}
